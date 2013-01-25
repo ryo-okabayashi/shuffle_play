9a91b1427d366e5a1eb59b8d3378abbad758e7cd
Ryo Okabayashi <okabayashi@dig-in.jp>
2013-01-25 14:02:50 +0900

    first

---
+++ shuffle_play.cpp
@@ -0,0 +1,58 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <unistd.h>
+
+using namespace std;
+
+char * play_random_flac(const char * dirname) {
+	DIR *dp;
+	struct stat st;
+	struct dirent *dir;
+	dp = opendir(dirname);
+	vector<string> dirs;
+	vector<string> flacs;
+
+	// ディレクトリとflacファイル抽出
+	if (dp != NULL) {
+		while ((dir = readdir(dp)) != NULL) {
+			if (dir->d_name[0] == '.') continue;
+			string path = string(dirname) + "/" + string(dir->d_name);
+			stat(path.c_str(), &st);
+			if (path.length() > 5 && path.substr(path.length() - 5) == ".flac") {
+				flacs.push_back(path);
+			}
+			if ((st.st_mode & S_IFMT) == S_IFDIR) {
+				dirs.push_back(path);
+			}
+		}
+
+		// flacが見つかれば再生 なければ再帰的に処理
+		srand(time(NULL));
+		if (flacs.size() > 0) {
+			cout << "play -> " << flacs[rand()%flacs.size()] << endl;
+			string cmd = "flac123 \"" + flacs[rand()%flacs.size()] + "\"";
+			int res = system(cmd.c_str());
+		} else if (dirs.size() > 0) {
+			play_random_flac(dirs[rand()%dirs.size()].c_str());
+		} else {
+			// ディレクトリ最深部までflacがないのでmain()で再スキャン
+			cout << "rescanning..." << endl;
+		}
+	}
+}
+
+int main(int argc, char * argv[]) {
+	if (argc < 2) {
+		cerr << "arg error" << endl;
+		exit(0);
+	}
+	while (1) {
+		play_random_flac(argv[1]);
+		sleep(1);
+	}
+}
